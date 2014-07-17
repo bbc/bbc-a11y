7c4692d7ef70413d6dbf8aeb66cca0be3b0b08ee
Matt Wynne <matt@mattwynne.net>
2014-07-17 22:28:47 +0100

    Rudimentary way to run more than one test

--- Rakefile
+++ Rakefile
@@ -1,13 +1,4 @@
-task default: [:ensure_url, :cucumber]
-
-task :ensure_url do
-  if ENV['URL']
-    puts "Testing against '#{ENV['URL']}'"
-    next
-  end
-  puts "Please enter the URL to test:"
-  ENV['URL'] = gets
-end
+task default: :cucumber
 
 require 'cucumber/rake/task'
 Cucumber::Rake::Task.new
--- lib/bbc/a11y.rb
+++ lib/bbc/a11y.rb
@@ -9,9 +9,22 @@ module BBC
       end
 
       def run
-        config.urls.each do |url|
-          exec("URL=#{url} cucumber /Users/matt/projects/a11y/standards-documentation -r /Users/matt/projects/a11y/lib/bbc/a11y/cucumber")
+        pids = config.urls.map do |url|
+          fork {
+            exec "URL=#{url} cucumber #{root}/standards-documentation -r #{root}/lib/bbc/a11y/cucumber"
+          }
         end
+        results = pids.map { |pid| Process.wait2(pid) }
+        puts '-' * 20
+        total = results.count
+        failed_count = results.count { |pid, status| status == 0 }
+        passed_count = total - failed_count
+        puts "#{total} tests run"
+        puts "#{failed_count} failed, #{passed_count} passed"
+      end
+
+      def root
+        File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
       end
 
       attr_reader :config

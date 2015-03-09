529406cf06bf7f0f7d93f88f89004ccc0aafc74c
Matt Wynne <matt@mattwynne.net>
2015-03-09 21:27:49 +0000

    Allow general page settings
    Closes #16
    
    Give it a spin @joshrp!

--- features/support/skipper.rb
+++ features/support/skipper.rb
@@ -1,5 +1,5 @@
 Before do |test_case|
-  if settings.scenarios_to_skip.any? { |name| test_case.name.match name }
+  if settings.skip_test_case?(test_case)
     skip_this_scenario "Configured to be skipped"
   end
 end
--- lib/bbc/a11y/configuration.rb
+++ lib/bbc/a11y/configuration.rb
@@ -21,22 +21,34 @@ module BBC
           :after_all_hooks,
           :pages
 
-        def initialize
-          @before_all_hooks = []
-          @after_all_hooks = []
-          @pages = []
+        def initialize(before_all_hooks = [], after_all_hooks = [], pages = [])
+          @before_all_hooks = before_all_hooks
+          @after_all_hooks = after_all_hooks
+          @pages = pages
           freeze
+        end
+
+        def with_pages(new_pages)
+          self.class.new(before_all_hooks, after_all_hooks, new_pages)
         end
       end
 
       class PageSettings
         attr_reader :url, :scenarios_to_skip, :world_extensions
 
-        def initialize(url)
+        def initialize(url, scenarios_to_skip = [], world_extensions = [])
           @url = url
-          @scenarios_to_skip = []
-          @world_extensions = []
+          @scenarios_to_skip = scenarios_to_skip
+          @world_extensions = world_extensions
           freeze
+        end
+
+        def skip_test_case?(test_case)
+          @scenarios_to_skip.any? { |pattern| pattern.match test_case.name }
+        end
+
+        def merge(other)
+          self.class.new(url, scenarios_to_skip + other.scenarios_to_skip, world_extensions + other.world_extensions)
         end
       end
 
@@ -44,7 +56,9 @@ module BBC
         attr_reader :settings
         def initialize(block)
           @settings = Settings.new
+          @general_page_settings = []
           instance_eval &block
+          @settings = settings.with_pages(apply_general_settings(settings.pages))
         end
 
         def before_all(&block)
@@ -57,6 +71,21 @@ module BBC
 
         def page(url, &block)
           settings.pages << PageDSL.new(url, block).settings
+        end
+
+        def for_pages_matching(url, &block)
+          @general_page_settings << PageDSL.new(url, block).settings
+        end
+
+        private
+
+        def apply_general_settings(all_page_settings)
+          all_page_settings.map { |page_settings|
+            matching_general_settings = @general_page_settings.select { |general_page_settings| general_page_settings.url.match page_settings.url }
+            matching_general_settings.reduce(page_settings) { |page_settings, general_page_settings|
+              page_settings.merge(general_page_settings)
+            }
+          }
         end
       end
 

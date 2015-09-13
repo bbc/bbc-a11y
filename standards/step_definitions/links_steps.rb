4ce106eeeabad0635c11d5e555e1b6b46798e17a
Shashikant86 <shashikant.jagtap@aol.co.uk>
2015-09-13 16:05:04 +0100

    Adding un-implemented Step definitions

---
+++ standards/step_definitions/links_steps.rb
@@ -0,0 +1,19 @@
+Given(/^the page has link$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the links should be underlined wth different color$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the link should have mouse\-over state$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the page has multiple links and buttons$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the each controls on the page elements used properly$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

ecd88bfca9fc4b74746553fe99f6762350d5b535
Josh Chisholm <joshuachisholm@gmail.com>
2015-12-28 10:41:40 +0000

    show xpaths in exactly one main heading standard

--- lib/bbc/a11y/standards/exactly_one_main_heading.rb
+++ lib/bbc/a11y/standards/exactly_one_main_heading.rb
@@ -7,10 +7,15 @@ module BBC
         end
 
         def call(errors)
-          count = @page.all('h1').size
-          if count != 1
+          main_headings = @page.all('h1')
+          count = main_headings.size
+          if count == 0
             errors << "A document must have exactly one main heading." +
-                      " Found #{count} h1 elements."
+                      " Found 0 h1 elements."
+          elsif count != 1
+            errors << "A document must have exactly one main heading." +
+                      " Found #{count} h1 elements:\n" +
+                      main_headings.map { |h1| "* #{h1.path}" }.join("\n")
           end
         end
 

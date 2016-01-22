2a648c19e13dec517d31bc2791ddb7f45871dc79
Matt Wynne <matt@mattwynne.net>
2016-01-22 14:33:27 +0000

    Remove old standards directory

--- standards/step_definitions/w3c_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I submit the page to the W3C Markup Validation Service$/) do
-  w3c.validate settings.url
-end
-
-Then(/^there should be no errors$/) do
-  expect(w3c.errors).to be_empty
-end
--- standards/support/world.rb
+++
@@ -1,3 +0,0 @@
-require 'bbc/a11y/cucumber_support'
-
-World(BBC::A11y::CucumberSupport)

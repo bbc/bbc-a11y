89da7dc33a5bc17fd01cf1ae4931bd5dbf2ecb7c
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-04-28 15:42:07 +0100

    add support for aria-labelledby

--- test/fieldsSpec.js
+++ test/fieldsSpec.js
@@ -40,12 +40,21 @@ describe('Fields must have labels or titles', function () {
     const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })
 
     expect(outcome.results[0].errors).to.eql([
-      ['Button with no text has no aria-label:', {'xpath': '/html/body/button', 'element': this.button }]
+      ['Button with no text has no aria-label or aria-labelledby:', {'xpath': '/html/body/button', 'element': this.button }]
     ])
   })
 
   it('should not return an error when a button with no text has an aria-label', async function () {
     $('body').html('<html><body><button class="arrows__chevron" aria-label="Scroll carousel right"></button></body></html>')
+    this.button = document.getElementsByTagName('button')[0]
+
+    const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })
+
+    expect(outcome.results[0].errors).to.eql([])
+  })
+
+  it('should not return an error when a button with no text has an aria-labelledby', async function () {
+    $('body').html('<html><body><div id="my_label">My label</div><button aria-labelledby="my_label"></button></body></html>')
     this.button = document.getElementsByTagName('button')[0]
 
     const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })

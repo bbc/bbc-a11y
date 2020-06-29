43ab1b212fa939c10abb5052b5aa05d07f72fcec
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-06-29 12:22:44 +0100

    check for image and text before checking aria labels

--- test/fieldsSpec.js
+++ test/fieldsSpec.js
@@ -33,19 +33,30 @@ describe('Fields must have labels or titles', function () {
     expect(outcome.results[0].errors).to.eql([])
   })
 
-  it('should return an error when a button with no text has no aria-label', async function () {
+  it('should return an error when a button has no text or no inline image', async function () {
     $('body').html('<html><body><button class="arrows__chevron"></button></body></html>')
     this.button = document.getElementsByTagName('button')[0]
 
     const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })
 
     expect(outcome.results[0].errors).to.eql([
-      ['Button with no text has no aria-label or aria-labelledby:', {'xpath': '/html/body/button', 'element': this.button }]
+      ['Button has no text or inline icon image:', {'xpath': '/html/body/button', 'element': this.button }]
     ])
   })
 
-  it('should not return an error when a button with no text has an aria-label', async function () {
-    $('body').html('<html><body><button class="arrows__chevron" aria-label="Scroll carousel right"></button></body></html>')
+  it('should return an error when a button with no text and an inline image has no aria-label or aria-labelledby', async function () {
+    $('body').html('<html><body><button class="arrows__chevron"><img src="some-icon.png" /></button></body></html>')
+    this.button = document.getElementsByTagName('button')[0]
+
+    const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })
+
+    expect(outcome.results[0].errors).to.eql([
+      ['Button with no text or inline icon image has no aria-label or aria-labelledby:', {'xpath': '/html/body/button', 'element': this.button }]
+    ])
+  })
+
+  it('should not return an error when a button with no text and an inline image has an aria-label', async function () {
+    $('body').html('<html><body><button class="arrows__chevron" aria-label="Scroll carousel right"><img src="some-icon.png" /></button></body></html>')
     this.button = document.getElementsByTagName('button')[0]
 
     const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })
@@ -53,8 +64,8 @@ describe('Fields must have labels or titles', function () {
     expect(outcome.results[0].errors).to.eql([])
   })
 
-  it('should not return an error when a button with no text has an aria-labelledby', async function () {
-    $('body').html('<html><body><div id="my_label">My label</div><button aria-labelledby="my_label"></button></body></html>')
+  it('should not return an error when a button with no text and inline image has an aria-labelledby', async function () {
+    $('body').html('<html><body><div id="my_label">My label</div><button aria-labelledby="my_label"><img src="some-icon.png" /></button></body></html>')
     this.button = document.getElementsByTagName('button')[0]
 
     const outcome = await a11y.test({ only: ['Forms: Labelling form controls: Fields must have labels or titles'] })

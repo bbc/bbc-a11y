4bc39e26c7f774253902c3c184ba957329492180
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-06-30 08:48:24 +0100

    tidy ups

--- test/fieldsSpec.js
+++ test/fieldsSpec.js
@@ -6,8 +6,9 @@ var $ = require('jquery');
 describe('Fields', function () {
   describe('Accessible fields', function () {
     it('should not fail when a visible input element has a title', async function () {
-      $('body').html('<html><body><input title="input"></body></html>');
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<input title="input">';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
 
       const outcome = await a11y.test({
         only: [
@@ -19,10 +20,9 @@ describe('Fields', function () {
     });
 
     it('should not fail when a visible input element is of type image and has a src and alt attribute', async function () {
-      $('body').html(
-        '<html><body><input type="image" src="next-icon.svg" alt="Next"></body></html>'
-      );
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<input type="image" src="next-icon.svg" alt="Next">';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
 
       const outcome = await a11y.test({
         only: [
@@ -34,10 +34,10 @@ describe('Fields', function () {
     });
 
     it('should not fail when a visible input element has a <label> element referring to it', async function () {
-      $('body').html(
-        '<html><body><label for="input">Label</label><input id="input"></body></html>'
-      );
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<label for="input">Label</label><input id="input">';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('input')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -49,10 +49,10 @@ describe('Fields', function () {
     });
 
     it('should not fail when a button with no text and an inline image has an aria-label', async function () {
-      $('body').html(
-        '<html><body><button class="arrows__chevron" aria-label="Scroll carousel right"><img src="some-icon.png" /></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<button class="arrows__chevron" aria-label="Scroll carousel right"><img src="some-icon.png" /></button>';
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -64,10 +64,11 @@ describe('Fields', function () {
     });
 
     it('should not fail when a button with no text and inline image has an aria-labelledby', async function () {
-      $('body').html(
-        '<html><body><div id="my_label">My label</div><button aria-labelledby="my_label"><img src="some-icon.png" /></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<div id="my_label">My label</div><button aria-labelledby="my_label"><img src="some-icon.png" /></button>';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -79,10 +80,11 @@ describe('Fields', function () {
     });
 
     it('should not fail when a button with no text and background image CSS has an aria-label', async function () {
-      $('body').html(
-        '<html><body><div id="my_label">My label</div><button style="background-image: url(\'next-icon.svg\')" aria-label="Next"></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<div id="my_label">My label</div><button style="background-image: url(\'next-icon.svg\')" aria-label="Next"></button>';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -94,10 +96,11 @@ describe('Fields', function () {
     });
 
     it('should not fail when a button with no text and background image CSS has an aria-labelledby', async function () {
-      $('body').html(
-        '<html><body><div id="my_label">My label</div><button style="background-image: url(\'next-icon.svg\')" aria-labelledby="Next"></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<div id="my_label">My label</div><button style="background-image: url(\'next-icon.svg\')" aria-labelledby="Next"></button>';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -111,8 +114,10 @@ describe('Fields', function () {
 
   describe('Not accessible fields', function () {
     it('should fail when a visible input element has no title', async function () {
-      $('body').html('<html><body><input></body></html>');
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<input>';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('input')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -123,16 +128,16 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Field has no label or title attribute:',
-          { xpath: '/html/body/input', element: this.field },
+          { xpath: '/html/body/input', element: el },
         ],
       ]);
     });
 
     it('should fail when a visible input element is of type image and has no src', async function () {
-      $('body').html(
-        '<html><body><input type="image" alt="Next" /></body></html>'
-      );
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<input type="image" alt="Next" />';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('input')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -143,16 +148,16 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Field with type of image has no src:',
-          { xpath: '/html/body/input', element: this.field },
+          { xpath: '/html/body/input', element: el },
         ],
       ]);
     });
 
     it('should fail when a visible input element is of type image and has no alt attribute', async function () {
-      $('body').html(
-        '<html><body><input type="image" src="next-icon.svg" /></body></html>'
-      );
-      this.field = document.getElementsByTagName('input')[0];
+      var testNode = '<input type="image" src="next-icon.svg" />';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('input')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -163,16 +168,16 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Field with type of image has no alt text:',
-          { xpath: '/html/body/input', element: this.field },
+          { xpath: '/html/body/input', element: el },
         ],
       ]);
     });
 
     it('should fail when a button has no text or no image', async function () {
-      $('body').html(
-        '<html><body><button class="arrows__chevron"></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode = '<button class="arrows__chevron"></button>';
+
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -183,16 +188,16 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Button has no text or image:',
-          { xpath: '/html/body/button', element: this.button },
+          { xpath: '/html/body/button', element: el },
         ],
       ]);
     });
 
     it('should fail when a button with no text and an inline image has no aria-label or aria-labelledby', async function () {
-      $('body').html(
-        '<html><body><button class="arrows__chevron"><img src="some-icon.png" /></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<button class="arrows__chevron"><img src="some-icon.png" /></button>';
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -203,16 +208,16 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Button with inline icon image has no aria-label or aria-labelledby:',
-          { xpath: '/html/body/button', element: this.button },
+          { xpath: '/html/body/button', element: el },
         ],
       ]);
     });
 
     it('should fail when a button with no text and a background image has no aria-label or aria-labelledby', async function () {
-      $('body').html(
-        '<html><body><button style="background-image: url(\'next-icon.svg\')"></button></body></html>'
-      );
-      this.button = document.getElementsByTagName('button')[0];
+      var testNode =
+        '<button style="background-image: url(\'next-icon.svg\')"></button>';
+      $('body').html('<html><body>' + testNode + '</body></html>');
+      var el = document.getElementsByTagName('button')[0];
 
       const outcome = await a11y.test({
         only: [
@@ -223,7 +228,7 @@ describe('Fields', function () {
       expect(outcome.results[0].errors).to.eql([
         [
           'Button with inline icon image has no aria-label or aria-labelledby:',
-          { xpath: '/html/body/button', element: this.button },
+          { xpath: '/html/body/button', element: el },
         ],
       ]);
     });
